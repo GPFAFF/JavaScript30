3b064240e0c5d5edbc41c055b58ebffc5879a9d0
Greg Pfaff <greg.pfaff@razorfish.com>
2017-10-07 15:48:24 -0500

    adding mlb fetch scoreboard

--- Fetch/index.js
+++ Fetch/index.js
@@ -15,19 +15,28 @@ fetch(url)
 		const game_data = data.games;
 		return game_data.map((game) => {
 
+			console.log(game);
 			let game_container = createNode('div')
-			let home_team = createNode('h3')
-			let away_team = createNode('h3')
+			let home_team = createNode('div')
+			let home_score = createNode('p')
+			let away_team = createNode('div')
+			let away_score = createNode('p')
+
 
 			game_container.classList.add('game');
 			home_team.classList.add('team');
 			away_team.classList.add('team');
 
 			home_team.innerHTML = `${game.home_team_city} ${game.home_team_name}`
+			home_score.innerHTML = `${game.linescore.r.home}`
+			console.log(game.linescore.r.home);
+			away_score.innerHTML = `${game.linescore.r.away}`
 			away_team.innerHTML = `${game.away_team_city} ${game.away_team_name}`
 
 			append(game_container, home_team);
+			append(home_team, home_score);
 			append(game_container, away_team);
+			append(away_team, away_score);
 			append(games, game_container);
 		})
 	})
@@ -36,3 +45,36 @@ fetch(url)
 	});
 
 	console.log("LOADED");
+
+// 	//const url = 'https://randomuser.me/api';
+// // The data we are going to send in our request
+// let data = {
+//     name: 'Sara'
+// }
+// // The parameters we are gonna pass to the fetch function
+// let fetchData = {
+//     method: 'POST',
+//     body: data,
+//     headers: new Headers()
+// }
+// fetch(url, fetchData)
+// .then(function() {
+//     // Handle response you get from the server
+// });
+
+// const url = 'https://randomuser.me/api';
+// // The data we are going to send in our request
+// let data = {
+//     name: 'Sara'
+// }
+// // Create our request constructor with all the parameters we need
+// var request = new Request(url, {
+//     method: 'POST',
+//     body: data,
+//     headers: new Headers()
+// });
+
+// fetch(request)
+// .then(function() {
+//     // Handle response we get from the API
+// })
