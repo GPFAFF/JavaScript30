2495368aced7a90a2af7261265b11389322ddcc4
Greg Pfaff <greg.pfaff@razorfish.com>
2017-11-10 17:01:58 -0600

    adding grid support

--- Carousel/index.js
+++ Carousel/index.js
@@ -10,8 +10,8 @@ const activeCards = (event) => {
         card.style.zIndex = "1"
         card.classList.add('active');
         card.setAttribute('aria-selected', 'true');
-        const random = Math.random() * 100 - 50;
-        card.style.transform = `rotate(${random}deg) scale(1.25)`
+        const random = Math.random() * 20 - 10;
+        card.style.transform = `rotate(${random}deg) scale(1.15)`
     } else {
         card.style.zIndex = "0"
         card.classList.remove('active');
