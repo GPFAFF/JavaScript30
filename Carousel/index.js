a2bee95ce51eecfcc2883f519244800656876134
Greg Pfaff <greg.pfaff@razorfish.com>
2017-11-10 15:07:07 -0600

    i stink

---
+++ Carousel/index.js
@@ -0,0 +1,70 @@
+const buttons = document.querySelectorAll('.page');
+
+
+const activeButtons = (e) => {
+  let active = false;
+  console.log("i hate js")
+  console.log(active);
+  if (active == true) {
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].classList.remove('active');
+    }
+  const target = e.target;
+  target.classList.add('active');
+
+  }
+  active = true;
+}
+
+buttons.forEach(function (event) {
+  event.addEventListener('click', activeButtons);
+})
+
+
+
+// var $carousel = $(".js-carousel");
+// var $carouselList = $(".js-carousel-list");
+// var $carouselItem = $(".js-carousel-item");
+
+// var $btnNext = $(".js-carousel-btnNext");
+// var $btnPrev = $(".js-carousel-btnPrev");
+
+// var $pagItem = $(".js-carousel-pagItem");
+// var $btnPag = $(".js-carousel-btnPag");
+
+// var activeIndex = 0;
+// var maxIndex = $carouselItem.length - 1;
+
+// var layout = function () {
+//   $carouselItem.attr("aria-hidden", "true");
+//   $carouselItem.eq(activeIndex).attr("aria-hidden", "false");
+
+//   $btnPag.attr("aria-selected", "false");
+//   $btnPag.eq(activeIndex).attr("aria-selected", "true");
+// };
+
+// var onNext = function () {
+//   if (activeIndex !== maxIndex) {
+//     activeIndex++;
+//     layout();
+//   }
+// };
+
+// var onPrev = function () {
+//   if (activeIndex !== 0) {
+//     activeIndex--;
+//     layout();
+//   }
+// };
+
+// var onPag = function (event) {
+//   element = event.currentTarget;
+//   activeIndex = $btnPag.index(element);
+//   layout();
+// };
+
+// $btnNext.on("click", onNext);
+// $btnPrev.on("click", onPrev);
+// $btnPag.on("click", onPag);
+
+// layout();
